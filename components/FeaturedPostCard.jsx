16434d67b5f944225c10cc1dc13bd485bb802e67
Shantonu <shantonupatiil08@gmail.com>
2022-11-25 01:05:44 +0530

    fixed carousel bug

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const FeaturedPostCard = ({ post }) => (
-  <div className="relative h-72 w-72 makebig">
+  <div className="relative h-72 w-72">
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
@@ -13,7 +13,7 @@ const FeaturedPostCard = ({ post }) => (
         <Image
           unoptimized
           alt={post.author.name}
-          
+
           height="30px"
           width="30px"
           className="align-middle drop-shadow-lg rounded-full techfont"
